e31184a7482d92023c13387126fe80efd6ef1e09
846005303@qq.com <yangjie>
2018-07-19 23:02:19 +0800

    Initial commit

---
+++ src/main/java/com/cedu/nekopapa/NekopapaApplication.java
@@ -0,0 +1,12 @@
+package com.cedu.nekopapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NekopapaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NekopapaApplication.class, args);
+	}
+}
